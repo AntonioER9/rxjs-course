3b20b356acd52d90561a21da2da8d0158474cb51
Antonio Espinoza <anespinoza@baufest.com>
2024-05-27 13:03:17 -0400

    add comments

--- src/observables/08-asyncScheduler.ts
+++ src/observables/08-asyncScheduler.ts
@@ -6,11 +6,11 @@ import { asyncScheduler } from 'rxjs';
 const saludar  = () => console.log('Hola Mundo');
 const saludar2 = nombre => console.log(`Hola ${ nombre }`);
 
-// asyncScheduler.schedule( saludar, 2000 );
-// asyncScheduler.schedule( saludar2, 2000, 'Fernando' );
+// asyncScheduler.schedule( saludar, 2000 ); //setTimeout
+// asyncScheduler.schedule( saludar2, 2000, 'Fernando' ); //setTimeout
 
 
- const subs = asyncScheduler.schedule( function(state){
+ const subs = asyncScheduler.schedule( function(state){ //setInterval
 
     console.log('state', state);
 
@@ -24,11 +24,3 @@ const saludar2 = nombre => console.log(`Hola ${ nombre }`);
 // }, 6000);
 
 asyncScheduler.schedule( ()=> subs.unsubscribe(), 6000 );
-
-
-
-
-
-
-
-
