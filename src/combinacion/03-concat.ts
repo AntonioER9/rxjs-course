597c0b3c34990404be79e6591cb226c891f83a27
Antonio Espinoza <anespinoza@baufest.com>
2024-05-28 17:22:01 -0400

    add more combination operators

---
+++ src/combinacion/03-concat.ts
@@ -0,0 +1,13 @@
+import { interval, concat, of } from 'rxjs';
+import { concatMap, take } from 'rxjs/operators';
+
+
+
+const interval$ = interval(1000);
+
+concat( //función
+    interval$.pipe( take(3) ),
+    interval$.pipe( take(2) ),
+    of(1)
+).subscribe( console.log  )
+
